f217253acfb898852b637ee87c48379a9ec6aabd
EduardoOsteicoechea <eduardooost@gmail.com>
2025-08-14 17:03:06 -0400

    update

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {    
-    let app = Router::new().route("/", get(root_handler));
+    let app = Router::new().route("/api", get(root_handler));
     let addr = SocketAddr::from(([0, 0, 0, 0], 7000));
     let listener = TcpListener::bind(addr).await.unwrap();
     println!("Blazing fast API listening on http://{}", addr);
